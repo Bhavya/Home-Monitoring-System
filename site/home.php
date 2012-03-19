e764b0b0790a0fefce600023035ae13238437acf
Bhavya Kashyap <bhkashyap@gmail.com>
2012-03-18 20:34:07 -0400

    adding menu icons

--- site/home.php
+++ site/home.php
@@ -29,12 +29,38 @@
       </div>
     </div>
     <div class="content">
+    	<div class="sidemenu">
+    		Home Security <hr/>
+    		<table class="menu">
+    			<tr>
+    				<td> <img src="img/door.png"> <br/> Doors and Windows</td>
+    				<td> <img src="img/motion.png"> <br/> Motion </td>
+    				<td> <img src="img/floodlight.png"> <br/> FLoodlights</td>
+    			</tr>
+    		</table >
+    		Family Monitoring <hr/>
+    		 <table class="menu">
+    			<tr>
+    				<td></td>
+    				<td><img src="img/camera.png"> <br/> Cameras</td>
+    				<td></td>
+    			</tr>
+    		</table>
+    		Green Energy <hr/>
+    		<table class="menu">
+    			<tr>
+    				<td><img src="img/lights.png"> <br/> Lights</td>
+    				<td><img src="img/temperature.png"> <br/>Temperature</td>
+    				<td><img src="img/power.png"> <br/>Power</td>
+    			</tr>
+    		</table>
+    	</div>
     	<div class="welcome">
     		Welcome, <?php echo $firstname;?>!
     	</div>
     	<div class="updates">
     		<?php 
-    			registerLoggedEvent($house_id, date("F j, Y, g:i a"), 'Bedroom Lights Off');
+    			//registerLoggedEvent($house_id, date("F j, Y, g:i a"), 'Kitchen Lights Off');
     			renderAllLoggedEvents($house_id);
     		?> 
     	</div>
