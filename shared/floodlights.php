0066186ff239d2950ca57474e53deacdf9f87521
Adam A Flynn <adamaflynn@gmail.com>
2012-03-19 14:13:08 -0700

    Added model objects for temperature, lights, and floodlights

---
+++ shared/floodlights.php
@@ -0,0 +1,37 @@
+<?php
+class FloodlightsRecord {
+    private $_houseId;
+    private $_deviceId;
+    private $_timestamp;
+    private $_state;
+
+    public function getDevice() {
+        return $this->_deviceId;
+    }
+
+    public function getTimestamp() {
+        return $this->_timestamp;
+    }
+
+    public function getState() {
+        return $this->_state;
+    }
+
+    static public function load($houseId) {
+        $result = mysql_query("SELECT * FROM floodlights_data WHERE house_id = '". $houseId ."'") or die(mysql_error());
+
+        $records = array();
+
+        while ($row = mysql_fetch_array($result)) {
+            $lights = new FloodlightsRecord();
+            $lights->_houseId = $houseId;
+            $lights->_deviceId = $row['device_id'];
+            $lights->_timestamp = $row['timestamp'];
+            $lights->_state = $row['state'];
+
+            $records[] = $lights;
+       }
+
+        return $records;
+    }
+}
